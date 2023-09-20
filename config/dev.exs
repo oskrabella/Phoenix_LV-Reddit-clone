f5652942e6f36cfb487b228b008a4f9e930ac0fb
Dani Rodriguez A <danirodriguez@danis-air.mynetworksettings.com>
2023-09-20 14:55:16 -0400

    added validation, routing, db config

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :rddit_clone, RdditClone.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "rddit_clone_dev",
+  database: "danirodriguez",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
--- lib/rddit_clone/home/post.ex
+++ lib/rddit_clone/home/post.ex
@@ -4,9 +4,9 @@ defmodule RdditClone.Home.Post do
 
   schema "posts" do
     field :body, :string
-    field :username, :string
-    field :upvote_count, :integer
-    field :downvote_count, :integer
+    field :username, :string, default: "oscar_abella"
+    field :upvote_count, :integer, default: 0
+    field :downvote_count, :integer, default: 0
 
     timestamps()
   end
@@ -14,7 +14,8 @@ defmodule RdditClone.Home.Post do
   @doc false
   def changeset(post, attrs) do
     post
-    |> cast(attrs, [:username, :body, :upvote_count, :downvote_count])
-    |> validate_required([:username, :body, :upvote_count, :downvote_count])
+    |> cast(attrs, [:body])
+    |> validate_required([:body])
+    |> validate_length(:body, min: 2, max: 300)
   end
 end
